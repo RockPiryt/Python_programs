d12674a46a7f6967703c569c103cb6d2c4704e41
Paulina Kimak <rockpiryt@gmail.com>
2024-05-05 20:11:59 +0200

    'RockPiryt' - add test for ex2 file, create fixtures in conftest.py

--- zadanie_5_chess_game/conftest.py
+++ zadanie_5_chess_game/conftest.py
@@ -1,15 +1,35 @@
-# import pytest
+import pytest
+from ex1_create_board import create_empty_board, place_pawn, place_queen, display_board
+from ex2_move_linear import get_position_pawn, get_position_queens
 
-# @pytest.fixture()
-# def init_game_board():
-#     print("\nHere we got our chess board - we are in fixture")
-#     pawn_pos = (5,1)
-#     quens_pos = [(4,0), (5,0), (5,6)]
-#     chess_figure_pos = [pawn_pos, quens_pos]
-#     return chess_figure_pos
+# Create empty chess board
+@pytest.fixture()
+def empty_board():
+    print("\nTest empty chess board:")
+    empty_board = create_empty_board()
+    display_board(empty_board)
+    return empty_board
 
-# @pytest.fixture()
-# def setup_list():
-#     print("\n we are in fixture")
-#     city = ["Ala", "ma", "kota"]
-#     return city
+# Create board with one pawn and random numbers of queens
+@pytest.fixture()
+def chess_board():
+    print("\nTest chess board:")
+    chess_board = create_empty_board()
+    place_pawn(chess_board)
+    place_queen(chess_board)
+    display_board(chess_board)
+    return chess_board
+
+# Get location of pawn
+@pytest.fixture()
+def pawn_placement(chess_board):
+    pawn_location = get_position_pawn(chess_board)
+    print("\nPawn Placement:", pawn_location)
+    return pawn_location
+
+# Get locations of queens
+@pytest.fixture()
+def queen_placements(chess_board):
+    queen_locations = get_position_pawn(chess_board)
+    print("\nQueen Placement:", queen_locations)
+    return queen_locations
--- zadanie_5_chess_game/tests/test_ex1.py
+++ zadanie_5_chess_game/tests/test_ex1.py
@@ -1,74 +1,26 @@
-import pytest
-from ex1_create_board import create_empty_board, place_pawn, place_queen, display_board
+from ex1_create_board import  place_pawn, place_queen
 
-#_Info
-# grupowanie testów za pomocą mark
-# to disable warnings from pytest about markers-> register markers in pytest.ini
-# pytest -v –exitfirst - przeprowadzanie testów do failed
-# pytest -m 'placement' - puszczanie testów oznaczonych mark
-# pytest -v -k ”a1” - puszczanie testów o określonej nazwie
-# pytest -v –-maxfail=2 - określenie max ilości failów
-# pytest -v –s - żeby wyprintować coś podczas testu
-# @pytest.mark.parametrize("test_input",[1,2,3,4]) - określenie setów danych do testowania
-#fixtures – robią setup dla innych testów lub robią clean po przeprowadzonych testach
-    #• connection to db
-    #• open files
-#fixtures można zapisać w conftest.py – wtedy dostępne dla całego modułu
-
-#___________________________Scenariusze co będziemy testować
-# na planszy miało się pokazać n królowych - sprawdzam czy tyle się pojawiło
-# na planszy miał pojawić sie randomowo 1 pionek  - sprawdzam czy się pojawił
-
-# @pytest.mark.placement 
-# @pytest.mark.parametrize("test_input",[1,2,3,4])
-# def test_place_pawn(test_input):
-#     print("ulala")
-#     assert test_input>0
-
-# def test_place_queen():
-#     assert 5+5 == 11, 'failed test intentionally'
-
-# @pytest.mark.check
-# def test_check_figures(init_game_board):
-#     pawn_position = init_game_board[0]
-#     print(f"Pawn position: {pawn_position}")
-#     assert pawn_position == (5,1)
-
-# def test_getItem(setup_list):
-#     print(setup_list[0])
-#     assert setup_list[0] == "Ala"
+# Test Scenarios
+# 1. Test to check if is one pawn on test chess board
+# 2. Test to check if on chess board is max 5 queens 
 
 
-
-@pytest.fixture()
-def empty_board():
-    print("\nHere we got our empty chess board - we are in fixture")
-    empty_board = create_empty_board()
-    place_pawn(empty_board)
-    place_queen(empty_board)
-    display_board(empty_board)
-    return empty_board
-
-# @pytest.fixture()
-# def show_board():
-#     print("\nHere we display chess board - we are in fixture")
-#     display_board(empty_board)
-
-
-
+# Test to check if is one pawn on test chess board
 def test_one_pawn(empty_board):
-
+    place_pawn(empty_board)
     for row in range(8):
           for col in range(8):
                if empty_board[row][col] == '|P|':
                    is_pawn = "Yes"
     assert is_pawn == "Yes"
 
+# Test to check if on chess board is max 5 queens 
 def test_n_queens(empty_board):
+    place_queen(empty_board)
     count_queens = 0
     for row in range(8):
           for col in range(8):
                if empty_board[row][col] == '|H|':
                 count_queens += 1
-    print(count_queens)
+    print(f"Current count of queens on chess board: {count_queens}")
     assert count_queens < 6
